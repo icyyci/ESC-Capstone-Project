60c27ffeaa779f01460b98f58ea593cd9672a23b
Cornelius Yap <corneliusyap@yahoo.com>
2020-03-04 01:18:44 +0800

    Fixed bugs from initial commit

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const express = require('express');
 
 const server = express();
 server.use(express.static(path.join(__dirname, 'WebPages')));
-const port = process.eventNames.PORT || 5000;
+const port = process.env.PORT || 5000;
 server.set('port', port);
 server.listen(port, () => {
     console.log(`Express server started at ${port}`);
