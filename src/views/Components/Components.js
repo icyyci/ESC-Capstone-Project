fb13c7fa43e4c849d5679e25801250dd65dba748
icyyci <corneliusyap@yahoo.com>
2020-04-01 02:28:11 +0800

    Edited for implementation to server

--- src/views/Components/Components.js
+++ src/views/Components/Components.js
@@ -1,26 +1,150 @@
 import React from "react";
-// nodejs library that concatenates classes
-// react components for routing our app without refresh
-import { Link } from "react-router-dom";
+import axios from "axios";
 // @material-ui/core components
 import { makeStyles } from "@material-ui/core/styles";
-// @material-ui/icons
-// core components
-import Button from "components/CustomButtons/Button.js";
 
-import styles from "assets/jss/material-kit-react/views/components.js";
+// @material-ui/icons
+import Announcement from "@material-ui/icons/Announcement";
+import FormatQuote from "@material-ui/icons/FormatQuote";
+import Store from "@material-ui/icons/Store";
+
+// core components
+import GridContainer from "components/Grid/GridContainer.js";
+import GridItem from "components/Grid/GridItem.js";
+import NavPills from "components/NavPills/NavPills.js";
+import SectionTable from "./SectionTable"
+
+import styles from "assets/jss/material-kit-react/views/componentsSections/pillsStyle.js";
+import { Component } from 'react'
+import {withStyles} from '@material-ui/core/styles'
 
 const useStyles = makeStyles(styles);
 
-export default function Components(props) {
-  const classes = useStyles();
-  return (
-    <div>
-      <Link to={"/announcements"} className={classes.link}>
-            <Button color="primary" size="lg" simple>
-              Click Here to go to Announcements Page
-            </Button>
-      </Link>
-    </div>
-  );
+export class SectionPills extends Component {
+
+  constructor(props) {
+    super(props)
+  
+    this.state = {
+	   url: "",
+	   capstoneGrp: "",
+       dataAnnouncement: 'fetch announcement here',
+       dataCurrentAllocation: 'fetch current allocation map here',
+       dataLatestRequest: [ ]
+    }
+  }
+
+  useStyles = () => {
+    return useStyles();
+  }
+
+  
+  //FUNCTIONS TO FETCH DATA FROM SERVER COMPONENTDIDMOUNT OR WTV => DO this.setstate({dataLatestRequest: newdata})
+	componentDidMount() {
+	if (window.location.host == "localhost:5000") {
+		this.state.url = "http://" + window.location.host;
+	}
+	else {
+		this.state.url = "https://" + window.location.host;
+	}
+
+		axios.post(this.state.url + "/user", {request:"firstload"}).then(res => {
+		console.log(res.data.groupRequest);
+		this.state.capstoneGrp = "Capstone " + res.data.groupNumber;
+		var dataJson = res.data.groupRequest;
+		var dataRequest = [];
+		var index = 0;
+		var keyArray = ["Type of Prototype:", "Showcase Space Needed:",
+		"Size and Weight of Physical Prototype:",
+		"No. of Power Points Needed:",
+		"Pedestal(s):",
+		"Other Requests:"
+	]
+	for (var i in dataJson) {
+		var val = dataJson[i];
+		var key = keyArray[index];
+		index++;
+		var newJson = { };
+		newJson[key] = val;
+		dataRequest.push(newJson);
+	}
+	this.setState({dataLatestRequest: dataRequest});
+	console.log(this.state.dataLatestRequest);
+	})
+  }
+ 
+  
+  render() {
+
+    const classes = this.useStyles;
+    var dataAnnouncement = (
+      <span>
+        <p>
+          {this.state.dataAnnouncement}
+        </p>
+      </span>
+    )
+
+    var dataCurrentAllocation = (
+      <span>
+        <p>
+          {this.state.dataCurrentAllocation}
+        </p>
+      </span>
+    )
+
+    return (
+      <div className={classes.section}>
+        <div className={classes.container}>
+          <div id="navigation-pills">
+            <div className={classes.title}>
+                <h1>Dashboard</h1>
+            </div>
+            <div className={classes.subtitle}>
+                <h2>{this.state.capstoneGrp}</h2>
+            </div>
+  
+            <GridContainer>
+              <GridItem xs={12} sm={12} md={12} lg={12}>
+                <NavPills
+                  color="rose"
+                  horizontal={{
+                    tabsGrid: { xs: 12, sm: 4, md: 4 },
+                    contentGrid: { xs: 12, sm: 8, md: 8 }
+                  }}
+                  tabs={[
+                    {
+                      tabButton: "Announcement",
+                      tabIcon: Announcement,
+                      tabContent: (
+                        dataAnnouncement
+                      )
+                    },
+                    {
+                      tabButton: "Your Latest Request",
+                      tabIcon: FormatQuote,
+                      tabContent: (
+                        <span>
+                          <SectionTable data={this.state.dataLatestRequest}/>
+                        </span>
+                      )
+                    },
+                    {
+                      tabButton: "Your Current Allocation",
+                      tabIcon: Store,
+                      tabContent: (
+                        dataCurrentAllocation
+                      )
+                    }
+                  ]}
+                />
+              </GridItem>
+            </GridContainer>
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
+
+export default withStyles(styles)(SectionPills)
