fb304f6ddf39dcc6bdf53b8c738e39c3ed50b91e
Blojkmoc <56734722+Blojkmoc@users.noreply.github.com>
2020-04-06 17:32:13 +0800

    Add files via upload

---
+++ serviceWorker.test.js
@@ -0,0 +1,11 @@
+import { register, unregister } from "./serviceWorker";
+describe("register", () => {
+  it("should fail auto generated test", () => {
+    expect(register()).toBe(false);
+  });
+});
+describe("unregister", () => {
+  it("should fail auto generated test", () => {
+    expect(unregister()).toBe(false);
+  });
+});
